55c8f410beb6dcde373dab5b1b6d1fb7ea3ca955
Ou Ye <ou.ye.canada@hotmail.com>
2014-10-16 15:54:52 -0400

    basic activies for login register and main

---
+++ Phase2/MessageApp/src/com/example/messageapp/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.example.messageapp;
+
+import com.example.messageapp.RegisterActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	public void loginPerson(View v) {
+
+		EditText userNameText = (EditText) findViewById(R.id.user_name);
+		EditText userPasswordText = (EditText) findViewById(R.id.user_password);
+
+		String userName = userNameText.getText().toString();
+		String userPassword = userPasswordText.getText().toString();
+		
+		// What do we do with user name and password?
+		// Need to check with the server
+		
+		boolean checkUserAccount = true;
+		
+		if (checkUserAccount) {
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+		} else { //Send out a notification 
+			userNameText.setText("");
+			userPasswordText.setText("");
+			Toast msg = Toast.makeText(this, "Incorrect Username or Password",
+					Toast.LENGTH_LONG);
+			msg.show();
+		}
+	}
+
+	public void register(View v) {
+		Intent intent = new Intent(this, RegisterActivity.class);
+		startActivity(intent);
+	}
+
+}
