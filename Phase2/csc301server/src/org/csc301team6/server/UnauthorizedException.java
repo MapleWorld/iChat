b457e60380fcd6661047d0e0c03810754e06f0a3
g3rs-cdf <r.self@utoronto.ca>
2014-10-21 23:47:10 -0400

    Added message field to UnauthorizedException.java

--- Phase2/csc301server/src/org/csc301team6/server/UnauthorizedException.java
+++ Phase2/csc301server/src/org/csc301team6/server/UnauthorizedException.java
@@ -3,7 +3,14 @@ package org.csc301team6.server;
 //This gets thrown when an action fails due to lack of authorization
 
 public class UnauthorizedException extends Exception {
-	public UnauthorizedException(){
+	private String message;
+	
+	public UnauthorizedException(String msg){
 		super();
+		message = msg;
+	}
+	
+	public String getMessage() {
+		return message;
 	}
 }
