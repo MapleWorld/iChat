45941e0093d3fc208b2a327f1479d622d44b3b3c
g3rs-cdf <r.self@utoronto.ca>
2014-10-21 22:01:37 -0400

    Added functions for looking up user by ID and name

--- Phase2/csc301server/src/org/csc301team6/server/UserDTO.java
+++ Phase2/csc301server/src/org/csc301team6/server/UserDTO.java
@@ -44,4 +44,74 @@ public class UserDTO {
 			}
 		}	
 	}
+	
+	public static CSC301User fetchUserByID(long userid){
+		ConfigManager mgr = ConfigManager.getInstance();
+		Connection conn = null;
+		PreparedStatement ps;
+		ResultSet rs;
+		CSC301User user = null;
+		
+		try {
+			conn = DriverManager.getConnection(mgr.getJDBCURL());
+			ps = conn.prepareStatement("select * from user where id = ?");
+			ps.setLong(1, userid);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				user = new CSC301User();
+				user.setBanned(rs.getInt("banned") > 0);
+				user.setID(rs.getInt("id"));
+				user.setUsername(rs.getString("username"));
+			}
+			
+			rs.close();
+			ps.close();				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return user;
+	}
+	
+	public static CSC301User fetchUserByName(String name){
+		ConfigManager mgr = ConfigManager.getInstance();
+		Connection conn = null;
+		PreparedStatement ps;
+		ResultSet rs;
+		CSC301User user = null;
+		
+		try {
+			conn = DriverManager.getConnection(mgr.getJDBCURL());
+			ps = conn.prepareStatement("select * from user where username = ?");
+			ps.setString(1, name);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				user = new CSC301User();
+				user.setBanned(rs.getInt("banned") > 0);
+				user.setID(rs.getInt("id"));
+				user.setUsername(rs.getString("username"));
+			}
+			
+			rs.close();
+			ps.close();				
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return user;
+	}
 }
