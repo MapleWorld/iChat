f62c9bc7714dc98b6b09b33cffe2c49803b8de76
Yolo <ou.ye.canada@hotmail.com>
2014-10-25 15:53:22 -0400

    modifed the api level and added basic activities and class

--- Phase2/MessageApp/gen/com/example/messageapp/R.java
+++ Phase2/MessageApp/gen/com/example/messageapp/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -32,32 +37,46 @@ public final class R {
         public static final int user_password=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_register=0x7f030002;
+        public static final int activity_create_thread=0x7f030000;
+        public static final int activity_create_topic=0x7f030001;
+        public static final int activity_login=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_register=0x7f030004;
+        public static final int activity_view_thread=0x7f030005;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int register=0x7f070002;
+        public static final int create_thread=0x7f070000;
+        public static final int create_topic=0x7f070001;
+        public static final int login=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int register=0x7f070004;
+        public static final int view_thread=0x7f070005;
     }
     public static final class string {
-        public static final int action_settings=0x7f050007;
+        public static final int action_settings=0x7f05000a;
         public static final int app_name=0x7f050000;
-        public static final int back=0x7f05000c;
-        public static final int day=0x7f050009;
-        public static final int hello_world=0x7f05000e;
-        public static final int list=0x7f05000a;
+        public static final int back=0x7f05000f;
+        public static final int create_thread=0x7f050007;
+        public static final int create_topic=0x7f050009;
+        public static final int day=0x7f05000c;
+        public static final int hello_world=0x7f050011;
+        public static final int list=0x7f05000d;
         public static final int login_button=0x7f050002;
         public static final int patient_name=0x7f050001;
         public static final int register_button=0x7f050003;
-        public static final int save=0x7f05000b;
-        public static final int submitNewPatientData=0x7f050008;
-        public static final int title_activity_main=0x7f05000f;
-        public static final int title_activity_register=0x7f05000d;
-        public static final int user_email=0x7f050006;
+        public static final int save=0x7f05000e;
+        public static final int submitNewPatientData=0x7f05000b;
+        public static final int title_activity_create_thread=0x7f050016;
+        public static final int title_activity_create_topic=0x7f050017;
+        public static final int title_activity_main=0x7f050012;
+        public static final int title_activity_register=0x7f050010;
+        public static final int title_activity_view_thread=0x7f050015;
+        public static final int title_item_detail=0x7f050014;
+        public static final int title_item_list=0x7f050013;
+        public static final int user_logout=0x7f050006;
         public static final int user_name=0x7f050004;
         public static final int user_password=0x7f050005;
+        public static final int view_thread=0x7f050008;
     }
     public static final class style {
         /** 
--- Phase2/MessageApp/src/appControl/User.java
+++ Phase2/MessageApp/src/appControl/User.java
@@ -1,5 +1,41 @@
 package appControl;
 
 public class User {
+	
+	private String username;
+	private String password;
+	private String id;
+
+	public User(String username, String password, String id){
+		this.username = username;
+		this.password = password;
+		this.id = id;
+	}
+	
+	public String getUsername(){
+		return this.username;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public void setPassword(String Password){
+		this.password = Password;
+	}
+
+	
+	public String getID(){
+		return this.id;
+	}
+	
+	public void setID(String ID){
+		this.id = ID;
+	}
+
 
 }
