b89fb1493a296689c19b91e71ebd263c3cb3f5d7
g3rs-cdf <r.self@utoronto.ca>
2014-10-28 23:09:49 -0400

    Fixed issue with connection settings not getting applied

--- Phase2/MessageApp/src/com/example/messageapp/SettingsActivity.java
+++ Phase2/MessageApp/src/com/example/messageapp/SettingsActivity.java
@@ -1,19 +1,35 @@
 package com.example.messageapp;
 
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import appControl.CSC301ConnectionManager;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
 	}
 
+	@Override
+	protected void onResume(){
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
 	public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+		Log.e("com.example.messageapp","calling onSharedPreferenceChanged...!");
 		String serverAddress = PreferenceManager.getDefaultSharedPreferences(
 				this).getString("pref_server_address", "");
 		boolean serverIsHTTPS = PreferenceManager.getDefaultSharedPreferences(
