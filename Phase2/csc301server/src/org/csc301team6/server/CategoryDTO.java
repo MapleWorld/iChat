8eb17f52b35b446b64b184520be89e94038cd055
FrancesYu <yfz111@hotmail.com>
2014-11-09 13:42:55 -0500

    implement admin create category

--- Phase2/csc301server/src/org/csc301team6/server/CategoryDTO.java
+++ Phase2/csc301server/src/org/csc301team6/server/CategoryDTO.java
@@ -66,4 +66,73 @@ public class CategoryDTO {
 
 		return category_obj.toString();
 	}
+	
+	public static long createCategory(String sessionID, String name) throws UnauthorizedException {
+		long category_id = -1;
+		ConfigManager mgr = ConfigManager.getInstance();
+		Connection conn = null;
+		PreparedStatement ps;
+		ResultSet rs;
+		long userid;
+		CSC301User user;
+		String topics_values = "";
+
+		try {
+			userid = SessionDTO.getUserIDFromSessionID(sessionID);
+			user = UserDTO.fetchUserByID(userid);
+			
+			if (!user.isAdmin()) {
+				throw new UnauthorizedException(
+						"User is not Admin and cannot create category.");
+			}
+
+			conn = DriverManager.getConnection(mgr.getJDBCURL());
+
+			// We have multiple inserts taking place that must be atomic
+			conn.setAutoCommit(false);
+
+			ps = conn.prepareStatement(
+					"insert into category (name) " + "values (?)",
+					Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, name);
+
+			ps.executeUpdate();
+
+			rs = ps.getGeneratedKeys();
+
+			if (rs.next()) {
+				category_id = rs.getInt(1);
+			} else {
+				category_id = -1;
+			}
+
+			rs.close();
+			ps.close();
+		} catch (UnauthorizedException e) {
+			//Provided session ID is not valid
+			e.printStackTrace();
+			throw e;
+		} catch (SQLException se) {
+			//Error executing one of the updates
+			se.printStackTrace();
+			
+			try {
+				// Revert what we did if there is a failure
+				conn.rollback();
+			} catch (SQLException se2) {
+				se.printStackTrace();
+			}
+			
+			throw new UnauthorizedException("An error has occurred");
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return category_id;
+	}
 }
--- Phase2/csc301server/src/org/csc301team6/server/CategoryServlet.java
+++ Phase2/csc301server/src/org/csc301team6/server/CategoryServlet.java
@@ -1,11 +1,17 @@
 package org.csc301team6.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CategoryServlet extends HttpServlet {
 
@@ -25,6 +31,82 @@ public class CategoryServlet extends HttpServlet {
 		String responseText = CategoryDTO.getAllCategoriesAsJSON();
 
 		response.getWriter().println(responseText);
+	}
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		Pattern newTopicPattern = Pattern.compile("^\\/categories\\/create$");
+		Matcher newThreadMatcher = newTopicPattern.matcher(request
+				.getRequestURI());
+		JSONObject jResp;
+
+		if (newThreadMatcher.find()) {
+			doNewCategory(request, response);
+		} else {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			jResp = new JSONObject();
+			jResp.put("message", "Illegal request");
+			response.getWriter().println(jResp.toString());
+		}
+	}
+
+	
+	private HttpServletResponse doNewCategory(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		String line;
+		String jsonInput = "";
+		JSONObject jo;
+		BufferedReader br = request.getReader();
+		String category_name;
+		String sessionID;
+		JSONObject jResp;
+		long category_id = -1;
+
+		jResp = new JSONObject();
+
+		response.setContentType("application/json");
+
+		sessionID = request.getHeader("SESSIONID");
+
+		if (sessionID == null) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			jResp.put("success", false);
+			jResp.put("message", "No session token provided");
+		} else {
+
+			while ((line = br.readLine()) != null) {
+				jsonInput += line;
+			}
+
+			try {
+				jo = new JSONObject(jsonInput);
+				category_name = jo.getString("name");
+				category_id = CategoryDTO.createCategory(sessionID, category_name);
+
+				if (category_id < 0) {
+					response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+					jResp.put("success", false);
+					jResp.put("message", "Error creating category from provided name");
+				} else {
+					response.setStatus(HttpServletResponse.SC_OK);
+					jResp.put("success", true);
+					jResp.put("message", "Category created");
+					jResp.put("cat_id", category_id);
+				}
+
+			} catch (JSONException e) {
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				jResp.put("success", false);
+				jResp.put("message", "Error parsing request");
+			} catch (UnauthorizedException ue) {
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				jResp.put("success", false);
+				jResp.put("message", ue.getMessage());
+			}
+		}
+
+		response.getWriter().println(jResp.toString());
+		return response;
 	}
 
 }
